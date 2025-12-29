66812c7d0be5a04b6bbd2f4bd2b51f340f080cc0
dyingpotato890 <niranjayajayan2004@gmail.com>
2025-12-29 19:41:38 +0530

    add: vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import LandingPage from './pages/landing/landing'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
   return (
     <>
+      <Analytics/>
       <LandingPage/>
     </>
   )
